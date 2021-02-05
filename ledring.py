4e95d8ef316e833a15374beb72c12f88b9bff2a0
skisbit <42703423+skisbit@users.noreply.github.com>
2021-02-05 18:58:16 +0100

    Add files via upload

---
+++ ledring.py
@@ -0,0 +1,148 @@
+# Setup
+import apa102
+import time
+import threading
+import sys
+try:
+    import queue as Queue
+except ImportError:
+    import Queue as Queue
+
+from gpiozero import LED
+from alexa_led_pattern import AlexaLedPattern
+from threading import Thread
+
+
+# Respeaker GPIO Data
+class Pixels:
+    PIXELS_N = 12
+
+    def __init__(self, pattern=AlexaLedPattern):
+        self.pattern = pattern(show=self.show)
+
+        self.dev = apa102.APA102(num_led=self.PIXELS_N)
+        
+        self.power = LED(5)
+        self.power.on()
+
+        self.queue = Queue.Queue()
+        self.thread = threading.Thread(target=self._run)
+        self.thread.daemon = True
+        self.thread.start()
+
+        self.last_direction = None
+
+    def wakeup(self, direction=0):
+        self.last_direction = direction
+        def f():
+            self.pattern.wakeup(direction)
+
+        self.put(f)
+
+    def listen(self):
+        if self.last_direction:
+            def f():
+                self.pattern.wakeup(self.last_direction)
+            self.put(f)
+        else:
+            self.put(self.pattern.listen)
+
+    def think(self):
+        self.put(self.pattern.think)
+
+    def speak(self):
+        self.put(self.pattern.speak)
+    
+    def error(self):
+        self.put(self.pattern.error)
+
+    def bootup(self):
+        self.put(self.pattern.bootup)
+
+    def off(self):
+        self.put(self.pattern.off)
+
+    def put(self, func):
+        self.pattern.stop = True
+        self.queue.put(func)
+
+    def _run(self):
+        while True:
+            func = self.queue.get()
+            self.pattern.stop = False
+            func()
+
+    def show(self, data):
+        for i in range(self.PIXELS_N):
+            self.dev.set_pixel(i, int(data[4*i + 1]), int(data[4*i + 2]), int(data[4*i + 3]))
+
+        self.dev.show()
+
+
+pixels = Pixels()
+
+
+# Loop Code:
+if __name__ == '__main__':
+    print '### Respeaker LED Ring Controller Script ###'
+    try:
+        while True:
+            import paho.mqtt.client as mqtt
+
+
+            def on_connect(client,userdata,flags,rc):
+                print("Connected to MQTT Server (Code:"+str(rc)+")")
+                
+                #client.subscribe("$SYS/#")
+                client.subscribe("ledring/wake")
+                client.subscribe("ledring/think")
+                client.subscribe("ledring/speak")
+                client.subscribe("ledring/off")
+                client.subscribe("hermes/asr/startListening")  
+                client.subscribe("hermes/hotword/alexa_raspberry-pi/detected")
+                client.subscribe("rhasspy/asr/default/default/audioCaptured")
+                pixels.bootup()                
+            def on_message(client,userdata,msg):
+                #print(msg.topic+" "+str(msg.payload)) #Debug
+                #sys.stdout.flush()
+                
+                # Manual Led Ring Activation
+                if msg.topic == "ledring/wake":
+                    pixels.wakeup()
+                if msg.topic == "ledring/think":
+                    pixels.think()
+                if msg.topic == 'ledring/speak':
+                    pixels.speak()
+                if msg.topic == 'ledring/error':
+                    pixels.error()
+                if msg.topic == 'ledring/off':
+                    pixels.off()
+                    
+                # Rhasspy Automatic Led Ring Activation
+                if msg.topic == "hermes/asr/startListening":
+                    pixels.wakeup()
+                if msg.topic == "hermes/hotword/alexa_raspberry-pi/detected":
+                    pixels.wakeup()
+                if msg.topic == "rhasspy/asr/default/default/audioCaptured":
+                    pixels.think()
+
+            def on_subscribe(client,userdata,result,mid):
+                   print("Subscribed to MQTT")
+
+
+            client = mqtt.Client()
+            client.username_pw_set(username="mqtt",password="mqtt")
+            client.on_connect = on_connect
+            client.on_message = on_message
+            client.connect("homeassistant.lan",1883,60)
+            client.on_subscribe = on_subscribe
+
+            client.loop_forever()            
+            
+            
+            
+
+    except KeyboardInterrupt:
+        print '\nProgram halted.'
+        sys.exit(0)
+    pixels.off()
