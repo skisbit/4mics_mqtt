79682f8b6c371672e3b1d2259d55f6c58e475bbb
skisbit <42703423+skisbit@users.noreply.github.com>
2021-02-24 12:49:33 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 ### MQTT Configuration ###
 
 mqtt_server = ""      # "1.2.3.4" or "mqtt.server"
-mqtt_username = ""    # can be empty
-mqtt_password = ""    # can be empty
+mqtt_username = ""    # Left empty if no username is set
+mqtt_password = ""    # Left empty if no password is set
 mqtt_port = ""        # Usually "1883"
 
 
