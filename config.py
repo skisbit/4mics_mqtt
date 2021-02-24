47619a37a1ce8d271b21d7d6ea6d92230dcef0f0
skisbit <42703423+skisbit@users.noreply.github.com>
2021-02-24 10:08:34 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,11 @@
+### MQTT Configuration ###
 mqtt_server = ""
 mqtt_username = ""
 mqtt_password = ""
 mqtt_port = ""
 
+### Rhasspy Configuration ###
+# Insert the site ID for this device
 rhasspy_siteid = ""
-
+# Insert the wakeword for this device
 wakeword = ""
